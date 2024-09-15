e06eaac70d0c8c0d6dc9e9d7e9b651de62a903c2
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2024-09-15 14:50:40 +0530

    my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config={
+  theme:{
+    gridTemplateColumns:{
+      'auto':'repeat(auto-fit,minmax(200px,1fr))'
+    },
+    //  fontFamily:{
+    //    Outfit:["Outfit", sans-serif],
+    //    Ovo:["Ovo", serif]
+    //  }
+    colors:{
+      lightHover:'#fcf4ff',
+      darkHover:'#2a004a',
+      darkTheme:'#11001F'
+    }
+  },
+  darkMode:'selector'
+}
